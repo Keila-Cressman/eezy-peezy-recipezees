5f14094f3f81eba4d27c667b09e68fa2f9fee27b
Keila Cressman <keila.cressman@gmail.com>
2025-03-04 15:46:16 -0700

    updated main page buttons

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -5,13 +5,13 @@ export default function PageTitle() {
       <div className="h-[1px] bg-gray-500 bg-opacity-20" />
       <div className="grid grid-cols-3 divide-x-2 text-center py-1">
         <button type="button" className="px-2 py-1">
-          <div className=" bg-blue-100 rounded-2xl">Home2</div>
+          <div className=" bg-blue-100 rounded-2xl">Home</div>
         </button>
         <button type="button" className="px-2 py-1">
           <div className=" bg-blue-100 rounded-2xl">Recipe Look Up</div>
         </button>
         <button type="button" className="px-2 py-1">
-          <div className=" bg-blue-100 rounded-2xl">Aboot</div>
+          <div className=" bg-blue-100 rounded-2xl">About</div>
         </button>
       </div>
     </div>
