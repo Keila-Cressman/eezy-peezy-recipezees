5717a290d07424f8a34bb04a279395fc7c440573
Keila Cressman <73181094+Keila-Cressman@users.noreply.github.com>
2025-03-11 07:19:22 -0700

    started route setup

--- src/layout/PageContents.tsx
+++ src/layout/PageContents.tsx
@@ -1,6 +1,21 @@
+import About from "../components/About"
 import Gallery from "../components/Gallery"
+import Home from "../components/Home"
 
 export default function PageContents() {
+  let PageView
+  switch (window.location.pathname) {
+    case "/":
+      PageView = <Home />
+      break
+    case "/about":
+      PageView = <About />
+      break
+    case "/gallery":
+      PageView = <Gallery />
+      break
+  }
+
   return (
     <div className="flex flex-col">
       <div className="flex flex-1 text-4xl text-center ">
--- src/layout/PageTitle.tsx
+++ src/layout/PageTitle.tsx
@@ -4,15 +4,10 @@ export default function PageTitle() {
       <div className="text-4xl text-center pb-1">Eezy Peezy Recipezees</div>
       <div className="h-[1px] bg-gray-500 bg-opacity-20" />
       <div className="grid grid-cols-3 divide-x-2 text-center py-1">
-        <button type="button" className="px-2 py-1">
-          <div className=" bg-blue-100 rounded-2xl">Home</div>
-        </button>
-        <button type="button" className="px-2 py-1">
-          <div className=" bg-blue-100 rounded-2xl">Recipe Look Up</div>
-        </button>
-        <button type="button" className="px-2 py-1">
-          <div className=" bg-blue-100 rounded-2xl">About</div>
-        </button>
+          <a href="/" className="px-2 py-1 bg-blue-100 rounded-2xl">Home</a>
+          <a href="/gallery" className="px-2 py-1 bg-blue-100 rounded-2xl">Recipe Cards</a>
+          <a href="/about" className="px-2 py-1 bg-blue-100 rounded-2xl">About</a>
+        
       </div>
     </div>
   )
