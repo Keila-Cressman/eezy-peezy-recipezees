768082c87aebbfaa84d9f999e206f11354cdb8a3
Keila Cressman <73181094+Keila-Cressman@users.noreply.github.com>
2025-03-22 22:06:00 -0700

    end point update

--- src/layout/PageContents.tsx
+++ src/layout/PageContents.tsx
@@ -4,20 +4,24 @@ import Home from "../components/Home"
 export default function PageContents() {
   let PageView
   switch (window.location.pathname) {
-    case ("/eezy-peezy-recipezees"):
-      PageView = <Home />
-      break
-    case "/about":
+    // case ("/eezy-peezy-recipezees"):
+    //   PageView = <Home />
+    //   break
+    case "/all-recipes":
       PageView = <Gallery recipeType="" />
       break
-      case ("/"):
+    case "/":
       PageView = <Home />
       break
+      
   }
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-1 text-4xl text-center ">{PageView}</div>
+      <div className="flex flex-1 text-4xl text-center ">
+        {PageView}
+        <Home />
+      </div>
     </div>
   )
 }
--- src/layout/PageTitle.tsx
+++ src/layout/PageTitle.tsx
@@ -5,7 +5,7 @@ export default function PageTitle() {
       <div className="h-[1px] bg-gray-500 bg-opacity-20" />
       <div className="grid grid-cols-2 divide-x-2 text-center py-1">
           <a href="/" className="px-2 py-1 bg-blue-100 rounded-2xl">Search Recipe</a>
-          <a href="/about" className="px-2 py-1 bg-blue-100 rounded-2xl">All Recipes</a>
+          <a href="/all-recipes" className="px-2 py-1 bg-blue-100 rounded-2xl">All Recipes</a>
         
       </div>
     </div>
