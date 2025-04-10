fff1b983517b8c7b51c863b769f33bd528a7b499
Keila Cressman <73181094+Keila-Cressman@users.noreply.github.com>
2025-04-10 07:19:34 -0700

    split cards into 2 cols and main page defaults to view all gallery

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -12,14 +12,38 @@ export type RecipeCardProps = {
 }
 
 export default function RecipeCard({ currRecipe }: RecipeCardProps) {
+  const midIndex = Math.ceil(currRecipe.length / 2)
+  const firstHalf = currRecipe.slice(0, midIndex)
+  const secondHalf = currRecipe.slice(midIndex)
+
   return (
-    <div>
-      {currRecipe.map((recipe) => (
-        <ul className="text-left">
-          <Gallery recipeType={recipe.name} className="rounded-xl h-60 my-4" />
-          <li>{recipe.name}</li>
-        </ul>
-      ))}
+    <div className="flex gap-4">
+      <div className="flex flex-col gap-2">
+        {firstHalf.map((recipe) => (
+          <div className="text-left bg-blue-100 rounded-xl w-[17rem] p-2">
+            <Gallery
+              recipeType={recipe.name}
+              className="rounded-xl h-36 my-4"
+            />
+            <div className="flex bg-green-300 overflow-hidden">
+              <span>{recipe.name}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-col gap-2">
+        {secondHalf.map((recipe) => (
+          <div className="text-left bg-blue-100 rounded-xl w-[17rem] p-2">
+            <Gallery
+              recipeType={recipe.name}
+              className="rounded-xl h-36 my-4"
+            />
+            <div className="flex bg-green-300 overflow-hidden">
+              <span>{recipe.name}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
