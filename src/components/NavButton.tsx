2bf76525fdb8e4d1617f30a050d9a65ac2fc081f
Keila Cressman <73181094+Keila-Cressman@users.noreply.github.com>
2025-04-17 16:44:17 -0700

    added highlight to button when clicked

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -1,18 +1,30 @@
-export type NavButtonProps={
+import { useEffect, useState } from "react"
+import { cn } from "../utils/cn"
+
+export type NavButtonProps = {
   title: string
   recipeType: string
   onClick: (recipeType: string) => void
   className: string
 }
 
-export default function NavButton({title, recipeType, onClick, className}:NavButtonProps){
-  return(
+export default function NavButton({
+  title,
+  recipeType,
+  onClick,
+  className,
+}: NavButtonProps) {
+  const [selected, setSelected] = useState(false) 
+  return (
     <button
-        type="button"
-        className={className}
-        onClick={() => onClick(recipeType)}
-      >
-        {title}
-      </button>
+      type="button"
+      className={cn(className, selected && "bg-blue-500")}
+      onClick={() => {
+        setSelected(!selected)
+        onClick(recipeType)
+      }}
+    >
+      {title}
+    </button>
   )
 }
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -11,85 +11,85 @@ export function SideNav({ onClick }: SideNavProps) {
         title="View all"
         recipeType="View_all"
         onClick={() => onClick("View_all")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Breakfast"
         recipeType="Breakfast"
         onClick={() => onClick("Breakfast")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Side"
         recipeType="Side"
         onClick={() => onClick("Side")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Main"
         recipeType="Main"
         onClick={() => onClick("Main")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Drink"
         recipeType="Drink"
         onClick={() => onClick("Drink")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Dessert"
         recipeType="Dessert"
         onClick={() => onClick("Dessert")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Thanksgiving"
         recipeType="Thanksgiving"
         onClick={() => onClick("Holiday_Thanksgiving")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Christmas"
         recipeType="Christmas"
         onClick={() => onClick("Holiday_Christmas")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Pre-requisite"
         recipeType="Pre-requisite"
         onClick={() => onClick("Pre-requisite")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Bakery"
         recipeType="Bakery"
         onClick={() => onClick("Bakery")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Jam"
         recipeType="Jam"
         onClick={() => onClick("Jam")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Sauce"
         recipeType="Sauce"
         onClick={() => onClick("Sauce")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Misc"
         recipeType="Misc"
         onClick={() => onClick("Misc")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
       <NavButton
         title="Tips & Tricks"
         recipeType="Tips_and_tricks"
         onClick={() => onClick("Tips_and_tricks")}
-        className="rounded bg-blue-100"
+        className="rounded bg-blue-100 hover:bg-sky-700"
       />
     </div>
   )
