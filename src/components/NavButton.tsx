d9bdc5c42d1d4a9a24beaa04dd0ac78d2322768c
Keila Cressman <73181094+Keila-Cressman@users.noreply.github.com>
2025-04-22 16:00:34 -0700

    highlight selected nav button done

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react"
 import { cn } from "../utils/cn"
 
 export type NavButtonProps = {
   title: string
   recipeType: string
   onClick: (recipeType: string) => void
-  isSelected: boolean
   className: string
 }
 
@@ -13,14 +11,12 @@ export default function NavButton({
   title,
   recipeType,
   onClick,
-  isSelected,
   className,
 }: NavButtonProps) {
-
   return (
     <button
       type="button"
-      className={cn(className, isSelected && "bg-blue-500")}
+      className={cn(className)}
       onClick={() => {
         onClick(recipeType)
       }}
