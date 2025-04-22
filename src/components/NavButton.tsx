809bf18b2116bfec049799fe5814e821676ca0b3
Keila Cressman <73181094+Keila-Cressman@users.noreply.github.com>
2025-04-22 07:21:10 -0700

    still working on selection

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -5,6 +5,7 @@ export type NavButtonProps = {
   title: string
   recipeType: string
   onClick: (recipeType: string) => void
+  isSelected: boolean
   className: string
 }
 
@@ -12,15 +13,15 @@ export default function NavButton({
   title,
   recipeType,
   onClick,
+  isSelected,
   className,
 }: NavButtonProps) {
-  const [selected, setSelected] = useState(false) 
+
   return (
     <button
       type="button"
-      className={cn(className, selected && "bg-blue-500")}
+      className={cn(className, isSelected && "bg-blue-500")}
       onClick={() => {
-        setSelected(!selected)
         onClick(recipeType)
       }}
     >
