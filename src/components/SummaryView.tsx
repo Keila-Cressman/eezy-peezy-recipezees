6e8c9e56d36b98d5aed26a4cb51b1a8e8f0a7b75
Keila Cressman <73181094+Keila-Cressman@users.noreply.github.com>
2025-06-17 16:34:29 -0700

    removed comments

--- src/components/SummaryView.tsx
+++ src/components/SummaryView.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import RecipeCard, { Recipe } from "./RecipeCard"
 import { SearchBar } from "./SearchBar"
-// import { ViewAll } from "./side-nav-bar/ViewAll"
 
 export type SummaryViewProps = {
   currRecipe: Recipe
@@ -11,15 +10,10 @@ export default function SummaryView({ currRecipe }: SummaryViewProps) {
   const [searchRecipeName, setSearchRecipeName] = useState("")
   return (
     <div className="flex flex-col gap-4 mt-2 pl-5">
-      {/* to be used for VIewALL */}
       <SearchBar
         searchRecipeName={searchRecipeName}
         setSearchRecipeName={setSearchRecipeName}
       />
-      {/* <ViewAll
-        searchFor={searchRecipeName}
-      /> */}
-      {/* to be used on side nav buttons */}
       <RecipeCard currRecipe={currRecipe as Recipe} searchFor={searchRecipeName}/>
     </div>
   )
