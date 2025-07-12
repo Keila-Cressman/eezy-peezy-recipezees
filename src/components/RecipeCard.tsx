1865ebd67243eb152411292530cbf7dac3f3df4b
Keila Cressman <73181094+Keila-Cressman@users.noreply.github.com>
2025-07-12 14:25:38 -0700

    expand card done

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -29,7 +29,7 @@ export default function RecipeCard({ searchFor, currRecipe }: RecipeCardProps) {
   }
   if (isMobile) {
     return (
-      <div className="flex gap-4 relative text-base font-semibold">
+      <div className="flex gap-4 relative text-base font-semibold pr-2">
         {openRecipeCard && (
           <ExpandRecipeCard
             recipeSelectedName={selectRecipe}
