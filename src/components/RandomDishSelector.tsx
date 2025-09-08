e13a83a375aeb7112c3b5290ff98db88b4631128
Keila Cressman <73181094+Keila-Cressman@users.noreply.github.com>
2025-09-07 17:26:44 -0700

    created page and button

---
+++ src/components/RandomDishSelector.tsx
@@ -0,0 +1,13 @@
+import { cn } from "../utils/cn"
+
+export function RandomDishSelector() {
+  return (
+    <div
+      className={cn(
+        "rounded bg-blue-100 flex-1"
+      )}
+    >
+      welcome!!!
+    </div>
+  )
+}
