77289483c3b8d6134f8b2950218836297a73d2b3
Keila Cressman <73181094+Keila-Cressman@users.noreply.github.com>
2026-02-09 19:54:34 -0700

    added "servings" for clarity

--- src/components/ServingSize.tsx
+++ src/components/ServingSize.tsx
@@ -12,43 +12,46 @@ export function ServingSize({ className }: servingSizeProps) {
   const [changeServing, setChangeServing] = useState(1)
   const isMobile = useMobileSize()
   return (
-    <div className={cn("flex", className)}>
-      <div className={cn("flex bg-blue-600 rounded-md")}>
-        <button
-          className={cn(
-            "bg-white m-2 rounded-md w-10 flex justify-center items-center",
-            isMobile && "w-7 m-1",
-          )}
-          type="button"
-          onClick={() => setChangeServing(changeServing - 1)}
-          disabled={changeServing <= 1}
-        >
-          <LeftArrowIcon
-            className={cn("h-10 text-blue-400", isMobile && "h-7")}
-          />
-        </button>
+    <div className={cn("flex text-base font-medium", className)}>
+      <div className="flex flex-col">
+        <div className="">Servings</div>
+        <div className={cn("flex bg-blue-600 rounded-md h-8")}>
+          <button
+            className={cn(
+              "bg-white m-1 rounded-md w-6 flex justify-center items-center",
+              isMobile && "w-7 m-1",
+            )}
+            type="button"
+            onClick={() => setChangeServing(changeServing - 1)}
+            disabled={changeServing <= 1}
+          >
+            <LeftArrowIcon
+              className={cn("h-10 text-blue-400", isMobile && "h-7")}
+            />
+          </button>
 
-        <div
-          className={cn(
-            "bg-white m-2 rounded-md flex items-center text-md w-10 justify-center",
-            isMobile && "m-1 text-xl",
-          )}
-        >
-          {changeServing}
+          <div
+            className={cn(
+              "bg-white m-1 rounded-md flex items-center text-md w-10 justify-center",
+              isMobile && "m-1 text-xl",
+            )}
+          >
+            {changeServing}
+          </div>
+
+          <button
+            className={cn(
+              "bg-white m-1 rounded-md w-6 flex justify-center items-center",
+              isMobile && "w-7 m-1",
+            )}
+            type="button"
+            onClick={() => setChangeServing(changeServing + 1)}
+          >
+            <RightArrowIcon
+              className={cn("h-10 text-blue-400", isMobile && "h-7")}
+            />
+          </button>
         </div>
-
-        <button
-          className={cn(
-            "bg-white m-2 rounded-md w-10 flex justify-center items-center",
-            isMobile && "w-7 m-1",
-          )}
-          type="button"
-          onClick={() => setChangeServing(changeServing + 1)}
-        >
-          <RightArrowIcon
-            className={cn("h-10 text-blue-400", isMobile && "h-7")}
-          />
-        </button>
       </div>
     </div>
   )
